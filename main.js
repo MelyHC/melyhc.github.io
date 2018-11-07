9ededcc657ed92f4a81b81961733740184e19fbb
MelyHC <mely.hidalgo.crespo@gmail.com>
2018-11-06 23:56:34 -0500

    Css acuarela y cards

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-// $('body').scrollspy({ target: '#navbar-example' });
+$('body').scrollspy({ target: '#navbar-example' });
 const navbar = document.querySelector('#navbarNavAltMarkup');
 const menu = document.querySelector('#menu');
 
-navbar.addEventListener('click', () => menu.click())
+// navbar.addEventListener('click', () => menu.click())
 
 $('[data-spy="scroll"]').on('activate.bs.scrollspy', function () {
 
