e78f8874f5938eb51d9e908903095ee6466b45a3
MelyHC <mely.hidalgo.crespo@gmail.com>
2018-11-16 13:15:48 -0500

    Arreglando css

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-$('body').scrollspy({ target: '#navbar-example' });
+
 const navbar = document.querySelector('#navbarNavAltMarkup');
 const menu = document.querySelector('#menu');
 
 // navbar.addEventListener('click', () => menu.click())
+$('body').scrollspy().init();
 
-$('[data-spy="scroll"]').on('activate.bs.scrollspy', function () {
-
+$('[data-spy="scroll"]').on('activate.bs.scrollspy', function (e) {
+ console.log(e)
 })
