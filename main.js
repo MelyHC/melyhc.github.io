ef1fe663732de0b7da4a0fa58b8c1135c7def59a
MelyHC <mely.hidalgo.crespo@gmail.com>
2018-11-17 19:52:18 -0500

    Scroll

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-
 const navbar = document.querySelector('#navbarNavAltMarkup');
 const menu = document.querySelector('#menu');
 
-// navbar.addEventListener('click', () => menu.click())
-$('body').scrollspy().init();
+$(document).ready(function () {
+  $('body').scrollspy({ target: ".navbar", offset: 56, scrollHeight: 56 });
+})
 
-$('[data-spy="scroll"]').on('activate.bs.scrollspy', function (e) {
- console.log(e)
+$('[data-spy="scroll"]').on('activate.bs.scrollspy', function () {
+})
+
+navbar.addEventListener('click', () => {
+  if (window.innerWidth < '768') menu.click()
 })
