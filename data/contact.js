ce02a90794aa960ed471db086f3ead9d43b75908
MelyHC <mely.hidalgo.crespo@gmail.com>
2024-09-30 21:50:15 -0500

    Sangría doc data contact

--- data/contact.js
+++ data/contact.js
@@ -1,31 +1,31 @@
 const contact = [
-    {
-        href: 'https://www.linkedin.com/in/melyhc/',
-        target: '_blank',
-        icon: 'fab fa-linkedin',
-        description: '/melyhc'
-    },
-    {
-        href: 'tel:929940441',
-        icon: 'fas fa-mobile-alt',
-        description: '929 940 441'
-    },
-    {
-        href: 'https://drive.google.com/open?id=1Du7QgsP_9FMmlA17LnAB0veaYaPJ4Rjo',
-        target: '_blank',
-        icon: 'fas fa-file',
-        description: 'Ver CV'
-    },
-    {
-        href: 'mailto:mely.hidalgo.crespo@gmail.com',
-        icon: 'fas fa-envelope',
-        description: 'mely.hidalgo.crespo@gmail.com',
-        classDes: 'mail pt-1'
-    },
-    {
-        href: 'https://github.com/MelyHC',
-        target: '_blank',
-        icon: 'fab fa-github',
-        description: '/MelyHC'
-    }
+  {
+    href: 'https://www.linkedin.com/in/melyhc/',
+    target: '_blank',
+    icon: 'fab fa-linkedin',
+    description: '/melyhc'
+  },
+  {
+    href: 'tel:929940441',
+    icon: 'fas fa-mobile-alt',
+    description: '929 940 441'
+  },
+  {
+    href: 'https://drive.google.com/open?id=1Du7QgsP_9FMmlA17LnAB0veaYaPJ4Rjo',
+    target: '_blank',
+    icon: 'fas fa-file',
+    description: 'Ver CV'
+  },
+  {
+    href: 'mailto:mely.hidalgo.crespo@gmail.com',
+    icon: 'fas fa-envelope',
+    description: 'mely.hidalgo.crespo@gmail.com',
+    classDes: 'mail pt-1'
+  },
+  {
+    href: 'https://github.com/MelyHC',
+    target: '_blank',
+    icon: 'fab fa-github',
+    description: '/MelyHC'
+  }
 ];
