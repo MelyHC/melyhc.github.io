3333e5c3ec356931658ea940af7d7d44f89e1a87
MelyHC <mely.hidalgo.crespo@gmail.com>
2026-01-06 20:56:08 -0500

    Agregando reglas eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+// @ts-check
+
+// import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig(
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  {
+    files: [["src/*", "**/.js"]],
+  },
+  {
+    files: ["**/*.js"],
+    rules: {
+      semi: "error",
+    },
+  },
+  // matches all files ending with .js except those in __tests
+  {
+    files: ["**/*.js"],
+    ignores: ["__tests/**"],
+    rules: {
+      "no-console": "error",
+    },
+  },
+);
